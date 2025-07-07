fd4cacd811abede292ecad9c7012c9a86aea4b4a
Nathan Pannell <n8tewolf@gmail.com>
2025-07-06 19:31:18 -0700

    refactor: tidy GetAlbumIds formatting

--- GetAlbumIds.fsx
+++ GetAlbumIds.fsx
@@ -8,6 +8,7 @@ open dotenv.net
 open FSharp.Data
 open FsHttp
 
+
 let getEnv name =
     match Environment.GetEnvironmentVariable name with
     | null | "" -> None
@@ -21,19 +22,21 @@ let requireEnv name =
     getEnv name |> Option.defaultWith (fun () -> failwithf "Missing environment variable: %s" name)
 
 
-
+let outputFilePath = "album_ids.txt"
 let sourcePlaylistId = "4KQDdglJ7HGcqNozbJTlM3" // 1000 Recordings to Hear Before You Die (Tom Moon): Album Index
 let spotifyBaseUri = "https://api.spotify.com/v1"
 type NewToken = JsonProvider<"sample_json/new_token.json">
 type PlaylistItems = JsonProvider<"sample_json/playlist_items.json">
 
+
+// Use refresh token to get a new access token for this session
+// https://developer.spotify.com/documentation/web-api/tutorials/refreshing-tokens
 let getAccessToken () : Async<string> = async {
 
     let refreshToken = requireEnv "refresh_token"
     let clientId = requireEnv "client_id"
     let clientSecret = requireEnv "client_secret"
 
-    printfn "Retrieving a new access token..."
     let! response =
         http {
             POST "https://accounts.spotify.com/api/token"
@@ -50,14 +53,14 @@ let getAccessToken () : Async<string> = async {
     |> Response.assert2xx
     |> Response.toText
     |> NewToken.Parse
-    |> fun newToken ->
-        printfn "Successfully found access token: %s" newToken.AccessToken
-        newToken.AccessToken
+    |> fun newToken -> newToken.AccessToken
 }
 
-let rec getPlaylistContents (accessToken: string) (offset: int) : Async<list<string>> = async {
+
+// Gets a list of tracks from the source playlist
+// Returns a list of the album Id for each track
+let rec getAlbumIds (accessToken: string) (offset: int) : Async<list<string>> = async {
     
-    printfn "Retrieving items %d-%d from playlist..." offset (offset + 49)
     let! response = 
         http {
             GET $"{spotifyBaseUri}/playlists/{sourcePlaylistId}/tracks"
@@ -80,18 +83,20 @@ let rec getPlaylistContents (accessToken: string) (offset: int) : Async<list<str
         |> Array.map _.Track.Album.Id
         |> Array.toList
 
+    // If there are more items that can be fetched, recurse
     if playlistItems.Total > offset + 50 then
-        let! rest = getPlaylistContents accessToken (offset+50)
+        let! rest = getAlbumIds accessToken (offset+50)
         return albumIds @ rest
     else
         return albumIds
 }
 
+
 let main _ =
     async {
         let! accessToken = getAccessToken ()
-        let! albumIds = getPlaylistContents accessToken 0
-        File.WriteAllLines("album_ids.txt", albumIds)
+        let! albumIds = getAlbumIds accessToken 0
+        File.WriteAllLines(outputFilePath, albumIds)
     }
     |> Async.RunSynchronously
     0
